d643133103290f999612b512ba59a7df79b90b8b
Gus <amuwonge99@gmail.com>
2026-05-12 20:50:21 +0100

    added validation check for cidr block

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,6 +4,11 @@ variable "region" {
 
 variable "vpc_cidr" {
   type        = string
+
+validation {
+    condition     = can(cidrhost(var.vpc_cidr, 0))
+    error_message = "vpc_cidr must be a valid CIDR block."
+  }
 }
 
 variable "azs" {
