80b5cdbb3881a5f476545c565b916ad25b7e588b
Gus <amuwonge99@gmail.com>
2026-05-13 23:25:14 +0100

    completed security-groups.tf

---
+++ terraform/security-groups.tf
@@ -0,0 +1,53 @@
+# ALB Security Group
+resource "aws_security_group" "alb" {
+  name        = "threat-modelling-tool-alb-security-group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+##################################################################
+
+# ECS Security Group
+resource "aws_security_group" "ecs" {
+  name        = "threat-modelling-tool-ecs-security-group"
+  description = "Security group for ECS tasks"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow traffic from ALB"
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
