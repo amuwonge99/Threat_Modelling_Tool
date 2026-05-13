fc995a5b3068a327ea2a7b939c8f08483cc5c87d
Gus <amuwonge99@gmail.com>
2026-05-13 21:42:52 +0100

    completed route53.tf with notes

---
+++ terraform/route53.tf
@@ -0,0 +1,34 @@
+# Data block to get the Route53 Hosted Zone for the domain
+data "aws_route53_zone" "main" {
+  name         = var.domain_name
+  private_zone = false
+}
+####################################################################
+
+# Route53 Record for the root domain
+resource "aws_route53_record" "root" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.app.dns_name
+    zone_id                = aws_lb.app.zone_id
+    evaluate_target_health = true
+  }
+}
+
+####################################################################
+
+# Route53 Record for the www subdomain
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.app.dns_name
+    zone_id                = aws_lb.app.zone_id
+    evaluate_target_health = true
+  }
+}
