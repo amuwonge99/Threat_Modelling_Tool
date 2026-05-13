bc83b66de47c1388006c7aa99120cc21fb24344d
Gus <amuwonge99@gmail.com>
2026-05-13 21:49:24 +0100

    completed iam.tf with notes

---
+++ terraform/iam.tf
@@ -0,0 +1,24 @@
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+######################################################################################
+
+# Attaching AmazonECSTaskExecutionRolePolicy to ECS Task Execution Role
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
