cd79cb1414144444043a246264edcc0fbec14ce6
Gus <amuwonge99@gmail.com>
2026-05-13 01:37:34 +0100

    s3 backend implemented

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "threat-modelling-tool-gm"
+    key            = "ecs-project/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "threat-modelling-tool-locks"
+    encrypt = true
+  }
+}
