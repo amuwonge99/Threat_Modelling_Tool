50e6ab9847a1b402f23e72c8c95108c745ec51fc
Gus <amuwonge99@gmail.com>
2026-05-15 01:53:57 +0100

    added policy & policy attachment for secrets manager

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -22,3 +22,32 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+#######################################################################################
+
+# Custom IAM Policy for ECS Tasks to access Secrets Manager
+resource "aws_iam_policy" "secrets_access" {
+  name = "ecs-secrets-access"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+
+        Resource = aws_secretsmanager_secret.app.arn
+      }
+    ]
+  })
+}
+#######################################################################
+
+# Attaching the custom secrets access policy to the ECS Task Execution Role
+resource "aws_iam_role_policy_attachment" "ecs_secrets" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.secrets_access.arn
+}
