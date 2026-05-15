b3c8cc561f28484794a8d5bb8ed88d6b850cabe7
Gus <amuwonge99@gmail.com>
2026-05-15 03:15:56 +0100

    removed secrets manager version

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,11 +1,3 @@
 resource "aws_secretsmanager_secret" "app" {
   name = "threat-modelling-tool-secrets"
 }
-
-resource "aws_secretsmanager_secret_version" "app" {
-  secret_id = aws_secretsmanager_secret.app.id
-
-  secret_string = jsonencode({
-    API_KEY = var.api_key
-  })
-}
