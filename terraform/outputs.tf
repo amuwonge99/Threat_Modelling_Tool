8d316bc85a4dadc43774d4014ed61e6987e31a99
Gus <amuwonge99@gmail.com>
2026-05-15 01:34:32 +0100

    added zone ID and alb dns name blocks

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -19,3 +19,13 @@ output "container_image" {
   value       = var.container_image
   description = "Docker image for the ECS service"
 }
+
+output "alb_dns_name" {
+  value       = aws_lb.app.dns_name
+  description = "DNS name of the ALB"
+}
+
+output "alb_zone_id" {
+  value       = aws_lb.app.zone_id
+  description = "Zone ID of the ALB"
+}
