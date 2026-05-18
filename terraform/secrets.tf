a031dc12eb7638634f3e54147f9ed9da8fe082a9
Gus <amuwonge99@gmail.com>
2026-05-18 13:05:24 +0100

    included secret id

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,3 +1,8 @@
 resource "aws_secretsmanager_secret" "app" {
   name = "threat-modelling-tool-secrets"
 }
+
+resource "aws_secretsmanager_secret_version" "api_key" {
+  secret_id     = aws_secretsmanager_secret.app.id
+  secret_string = var.api_key
+}
