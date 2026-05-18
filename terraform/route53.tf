39422a57e11e68b86080e0413c03b8298390700a
Gus <amuwonge99@gmail.com>
2026-05-18 10:57:13 +0100

    commented out route 53.tf

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,33 +1,33 @@
-# Route53 Hosted Zone
-resource "aws_route53_zone" "main" {
-  name = trimsuffix(var.domain_name, ".")
-}
-####################################################################
+# # Route53 Hosted Zone
+# resource "aws_route53_zone" "main" {
+#   name = trimsuffix(var.domain_name, ".")
+# }
+# ####################################################################
 
-# Route53 Record for the root domain
-resource "aws_route53_record" "root" {
-  zone_id = aws_route53_zone.main.zone_id
-  name = trimsuffix(var.domain_name, ".")
-  type    = "A"
+# # Route53 Record for the root domain
+# resource "aws_route53_record" "root" {
+#   zone_id = aws_route53_zone.main.zone_id
+#   name = trimsuffix(var.domain_name, ".")
+#   type    = "A"
 
-  alias {
-    name                   = aws_lb.app.dns_name
-    zone_id                = aws_lb.app.zone_id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = aws_lb.app.dns_name
+#     zone_id                = aws_lb.app.zone_id
+#     evaluate_target_health = true
+#   }
+# }
 
-####################################################################
+# ####################################################################
 
-# Route53 Record for the www subdomain
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "www.${trimsuffix(var.domain_name, ".")}"
-  type    = "A"
+# # Route53 Record for the www subdomain
+# resource "aws_route53_record" "www" {
+#   zone_id = aws_route53_zone.main.zone_id
+#   name    = "www.${trimsuffix(var.domain_name, ".")}"
+#   type    = "A"
 
-  alias {
-    name                   = aws_lb.app.dns_name
-    zone_id                = aws_lb.app.zone_id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = aws_lb.app.dns_name
+#     zone_id                = aws_lb.app.zone_id
+#     evaluate_target_health = true
+#   }
+# }
