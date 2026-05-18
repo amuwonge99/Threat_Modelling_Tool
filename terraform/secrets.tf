bd70339d94fa8b4e890d436a12eca63d412e3f9d
Gus <amuwonge99@gmail.com>
2026-05-18 13:47:34 +0100

    API key in github actions

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,7 +1,9 @@
 resource "aws_secretsmanager_secret" "app" {
   name = "threat-modelling-tool-secrets"
 }
+##################################################
 
+# Storing API key in Secrets Manager
 resource "aws_secretsmanager_secret_version" "api_key" {
   secret_id     = aws_secretsmanager_secret.app.id
   secret_string = var.api_key
